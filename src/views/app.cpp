3168b3151641ef1cee3892a8d50974527df180fb
cyh <328094057>
2022-07-15 14:27:40 +0800

    just for test add branch

---
+++ src/views/app.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cyh12 on 2022/7/15.
+//
+
+#include "app.h"
---
+++ src/views/app.h
@@ -0,0 +1,14 @@
+//
+// Created by cyh12 on 2022/7/15.
+//
+
+#ifndef ELECTRON_VUE_NOTES_APP_H
+#define ELECTRON_VUE_NOTES_APP_H
+
+
+class app {
+
+};
+
+
+#endif //ELECTRON_VUE_NOTES_APP_H
